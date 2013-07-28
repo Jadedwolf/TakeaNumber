f3e78215fc06bdcccdb34c9aa5eaeca7627a9541
Scott Mcleod <scott.r.mcleod@gmail.com>
2013-07-27 22:01:08 -0700

    Changed how the properties are loaded.

--- src/main/java/me/olddragon/takeanumber/Messages.java
+++ src/main/java/me/olddragon/takeanumber/Messages.java
@@ -6,7 +6,7 @@ import java.util.ResourceBundle;
 import org.bukkit.command.CommandSender;
 
 public class Messages {
-  private static final String BUNDLE_NAME = "me.olddragon.takeanumber.messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "translations/messages"; //$NON-NLS-1$
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
   private Messages() {}
